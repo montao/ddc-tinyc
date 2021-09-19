857d927363e9c9aaa713bb20adbe99ded76ac615
Niklas Rosencrantz <nik@kth.se>
2021-09-19 13:43:19 +0200

    Create evil Tcc

---
+++ tcc-evil/tinycc/test2c2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+char my_program[] =
+        "int fib(int n)\n"
+        "{\n"
+        "    if (n <= 2)\n"
+        "        return 1;\n"
+        "    else\n"
+        "        return fib(n-1) + fib(n-2);\n"
+        "}\n"
+        "\n"
+        "int foo(int n)\n"
+        "{\n"
+        "    printf(\"Hello World!\\n\");\n"
+        "    printf(\"fib(%d) = %d\\n\", n, fib(n));\n"
+        "    printf(\"add(%d, %d) = %d\\n\", n, 2 * n, add(n, 2 * n));\n"
+        "    return 0;\n"
+        "}\n";
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
+
+int bug_main() {
+    // printf() displays the string inside quotation
+    printf("Hello, World!");
+    return 0;
+}
